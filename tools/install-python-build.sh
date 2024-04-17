09adcfa165ae5bf2b4cc89df359ddc467a55e1e4
xxcosita3czxx <kokosove18@gmail.com>
2024-04-17 23:35:08 +0200

    python build

---
+++ tools/install-python-build.sh
@@ -0,0 +1,8 @@
+echo "Enter mountpoint: (full path)"
+read mount
+if [ -d /tmp/build-py ]; then
+    cd /tmp/build-py/Python-3.11.9
+    make install DESTDIR=$mount
+else
+    echo PYTHON BUILD NOT FOUND, RUN ./get-python-build.sh FIRST
+fi
