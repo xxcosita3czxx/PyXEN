8668401d5288046cfed5f4ef46cf668223a8781d
xxcosita3czxx <kokosove18@gmail.com>
2024-04-18 08:15:30 +0200

    Update install-python-build.sh

--- tools/install-python-build.sh
+++ tools/install-python-build.sh
@@ -3,6 +3,7 @@ read mount
 if [ -d /tmp/build-py ]; then
     cd /tmp/build-py/Python-3.11.9
     make install DESTDIR=$mount
+    echo done
 else
     echo PYTHON BUILD NOT FOUND, RUN ./get-python-build.sh FIRST
 fi
