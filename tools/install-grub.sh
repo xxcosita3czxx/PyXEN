949ee64568a15ebafea85979700b439624e586f4
xxcosita3czxx <kokosove18@gmail.com>
2024-04-19 20:25:31 +0200

    c

--- tools/install-grub.sh
+++ tools/install-grub.sh
@@ -2,5 +2,5 @@ echo 'Which device is system? (/dev/sdX)'
 read device
 echo 'Which mount is EFI?'
 read efipart
-grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=$
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=$efipart
 grub-install --target=i386-pc --boot-directory=/boot/ $device
