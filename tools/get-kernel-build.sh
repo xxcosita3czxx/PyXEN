f33125ea81e116d465d90ccff91063ad0b1515c9
xxcosita3czxx <kokosove18@gmail.com>
2024-04-19 21:33:58 +0200

    building

--- tools/get-kernel-build.sh
+++ tools/get-kernel-build.sh
@@ -3,3 +3,5 @@ cd /tmp/build-kernel
 wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.28.tar.xz
 tar -xf linux-6.6.28.tar.xz
 cd linux-6.6.28
+wget -O .config https://raw.githubusercontent.com/xxcosita3czxx/PyXEN/main/.config
+make -j$(nproc)
