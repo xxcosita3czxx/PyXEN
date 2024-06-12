56ae132c6fe56005b905d37702fd729d8d4bea1b
xxcosita3czxx <kokosove18@gmail.com>
2024-06-12 12:02:04 +0200

    some additions with initram and pysh

--- ROOTFS/bin/pysh.py
+++ ROOTFS/bin/pysh.py
@@ -1,2 +1,20 @@
-print('world: hi system')
-print('system: Hello world!')
+#! /bin/python3
+import os
+
+SHELL = "PyXEN -#:"
+PATH = "/bin/"
+
+if not SHELL.endswith(" "):
+    SHELL = SHELL + " "
+
+while True:
+    try:
+        inp = input(SHELL)
+    except KeyboardInterrupt:
+        print("KI")
+    except EOFError:
+        print("EOF")
+    try:
+        os.system(inp)
+    except:
+        print("NOT FOUND")
